4c4051e1e40435100a63ca1c8ec0f87c770ed3a7
grantw99 <grantw99@gmail.com>
2020-10-17 11:54:57 -0400

    Adding basic classifier

---
+++ simple_classifier.py
@@ -0,0 +1,18 @@
+import argparse
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+import cv2
+
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-i", "--image", required=True, help="Path to image file")
+
+model = load_model("classification.model")
+
+img = cv2.imread(args["image"])
+img = cv2.resize(img, (32, 32))
+img = img_to_array(img)
+
+preds = model.predict(img)
+
+print(preds)
